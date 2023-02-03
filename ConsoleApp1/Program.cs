0432b95cb6c2387aaea3d6e3fc745a58226d4f45
MauroDev <MauroDev@DESKTOP-SF6F6AD>
2023-02-03 08:29:42 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os numeros a serem formados");
+
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"O resultado da soma é {Soma(numero1, numero1)}");
+
+            Console.WriteLine("Aperte Enter para finalizar");
+
+            Console.ReadLine();
+
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+
+            return numero1 + numero2;
+
+        }
+
+    }
+
+
+
+}
